99694589f98333c620bf1fc5ee8348712e570262
Matthew Dapena-Tretter <m@tthewwithanm.com>
2014-07-02 19:47:53 -0400

    Create project skeleton

---
+++ test/tests.js
@@ -0,0 +1,11 @@
+/*globals chai, ReactTemplate, describe, it */
+'use strict';
+
+var assert = chai.assert;
+
+
+describe('react-template', function () {
+  it('passes a test', function() {
+    assert(true);
+  });
+});
