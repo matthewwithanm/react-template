60dcf7527b7ccc71b8c1a110851f7bd5d2448c35
Matthew Dapena-Tretter <m@tthewwithanm.com>
2014-07-02 20:13:32 -0400

    Add tests

--- test/tests.js
+++ test/tests.js
@@ -1,11 +1,79 @@
-/*globals chai, ReactTemplate, describe, it */
+/*globals chai, React, ReactTemplate, describe, it */
 'use strict';
 
 var assert = chai.assert;
+var div = React.DOM.div;
 
 
 describe('react-template', function () {
-  it('passes a test', function() {
-    assert(true);
+
+  it('creates a function', function() {
+    var template = ReactTemplate.create({
+      render: function() {}
+    });
+    assert.typeOf(template, 'function');
+  });
+
+  it('returns a template that calls the render function', function() {
+    var template = ReactTemplate.create({
+      render: function() {
+        return div();
+      }
+    });
+    assert(React.isValidComponent(template()));
+  });
+
+  it('receives props', function() {
+    var template = ReactTemplate.create({
+      render: function() {
+        assert.equal(this.props.hasProp, true);
+        return div();
+      }
+    });
+    template({hasProp: true});
+  });
+
+  it('applies default props', function() {
+    var template = ReactTemplate.create({
+      getDefaultProps: function() {
+        return {name: 'Matthew'};
+      },
+      render: function() {
+        assert.equal(this.props.name, 'Matthew');
+        return div();
+      }
+    });
+    template();
+  });
+
+  it('warns when a propType check fails', function() {
+    // Mock `console.warn`
+    var oldWarn = console.warn;
+    var newWarn = function() {
+      newWarn.called = true;
+    };
+    console.warn = newWarn;
+
+    var template = ReactTemplate.create({
+      propTypes: {
+        name: React.PropTypes.string
+      },
+      render: function() {
+        return div();
+      }
+    });
+    template({name: 1});
+    assert(newWarn.called);
+
+    // Fix `console.warn`
+    console.warn = oldWarn;
+  });
+
+  it('can be given a plain old function', function() {
+    var template = ReactTemplate.create(function() {
+      assert.equal(this.props.hasProp, true);
+      return div();
+    });
+    template({hasProp: true});
   });
 });
